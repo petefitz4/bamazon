a411a19eee0e3ae81f31eadb1cb59520952c03af
petefitz4 <petefitz4@gmail.com>
2017-12-04 19:59:19 -0600

    current files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,9 +13,9 @@ var connection = mysql.createConnection({
 //display all of the items available for sale. Include the ids, names, and prices of products for sale.
 connection.connect(function(err) {
   if (err) throw err;
-  connection.query("SELECT item_id, product_name, price FROM products", function (err2, result) {
+  connection.query("SELECT item_id, product_name, price FROM products", function (err2, results) {
     if (err2) throw err2;
-    console.log(result);
+    console.log(results);
   });
   buyProduct();
 });
@@ -41,26 +41,34 @@ function buyProduct() {
           type: "input",
           message: "How much would you like to buy?"
         }
-      ]).then(function(order)){
-
-
+      ])
+      .then(function(order){
+        //Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+        var item1 = order.item.toUpperCase();
+        var quantity1 = order.quantity;
+        console.log("Your item is: " + item1);
+        console.log("\n The quantity ordered is: " + quantity1);
         
-      }  
-  
-  
+        connection.query("SELECT * FROM products WHERE item_id= ?", [item1], function(err,results){
+          if (err) throw err;
+            if (results[0].stock_quantity - quantity1 >= 0){
+              //This means updating the SQL database to reflect the remaining quantity.
+              //Once the update goes through, show the customer the total cost of their purchase.
+              console.log("Your order will be processed. The total due is: $" + (order.quantity1 * results[0].price) + "/n Thank your for shopping with us!");
+              connection.query("UPDATE products SET stock_quantity=? WHERE id=?", [results[0].stock_quantity - quantity1, item_id],
+                function(err, results){
+                if(err) throw err;
+                  buyProduct();
+                });
+            }  
+            //If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+            else {
+              console.log ("Insufficent quantity in inventory");
+                connection.end();
+            }
+        })
+      })  
     });
-
-//Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-//If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-//However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//This means updating the SQL database to reflect the remaining quantity.
-//Once the update goes through, show the customer the total cost of their purchase.
-
-
-
-
 }
 
 //buyProduct();
